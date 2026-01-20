4998d8fbce7da0954c81550ef45ebdb68a4f5487
jacksonsilver <104522191+jacksonsilver@users.noreply.github.com>
2026-01-20 15:35:51 -0500

    Converted HTML to React app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import logo from '../assets/jacksonsweb_logo.png'
+
+function App() {
+  return (
+    <div className="container">
+      <img 
+        src={logo}
+        alt="title image saying 'jackson's web'"
+      />
+      <p>coming soon!!</p>
+      <a href="https://nekoweb.org/">
+        <img src="https://nekoweb.org/assets/buttons/button5.gif" alt="Nekoweb button" />
+      </a>
+    </div>
+  )
+}
+
+export default App
